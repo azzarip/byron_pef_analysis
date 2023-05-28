1afac0abab71a755fb2524030cf25e2e67f86cc4
Paride Azzari <paride.azzari@hest.ethz.ch>
2023-05-28 19:32:40 +0200

    analysis correctly reads file name and sort

--- src/analysis.py
+++ src/analysis.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def getTimes(filename):
+def extractTimes(filename):
     distance = 4.68/2
     data = pd.read_csv(f'data/{filename}.txt', header=None).drop(columns=[0]).T
     times=data.iloc[::2,0]
@@ -9,3 +9,21 @@ def getTimes(filename):
     high = abs(positions-distance).idxmin()
     time = high-low
     return time
+
+def getAverage(pre):
+    import pandas as pd
+    import os
+    df = pd.DataFrame()
+
+    for file in os.listdir("data"):
+        if file.startswith(pre):
+            print(file)
+            flow = file.replace(pre,'').replace('.txt','').lstrip('_')            
+            times = extractTimes(f'{pre}_{flow}')
+            mean = times.mean()
+            std = times.std()
+            df = pd.concat([df, pd.DataFrame([[flow, mean]])])
+                
+    df = df.rename(columns={0:'flow', 1:f'{pre}'}).set_index('flow')
+    df.index = pd.to_numeric(df.index, errors='coerce')
+    return df.sort_index()
